cedb83fdec1a9a9e35c01948fd8d19685d276630
Joe Streifel <josephstreifel@gmail.com>
2022-01-06 19:33:34 -0800

    creates and migrates Snacks model

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import CharField, TextField
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Snacks(models.Model):
+    name = CharField(max_length=64)
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description = TextField()
+
+    def __str__(self):
+        return self.name
